ab2a8995e40ec07afd2e367c1a24cfc367871113
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-07-27 00:21:45 +0300

    Update symbol_table.h

--- symbol_table.h
+++ symbol_table.h
@@ -10,24 +10,15 @@ typedef struct Symbol {
     struct Symbol* next;
 } Symbol;
  
-/*זה בעצם המסנה של הטבלת סמלים שמאפשר גישה באו של 1 ולא באו של אן*/
-typedef struct {                 
-    Symbol *cells[SYMBOL_TABLE_SIZE];
-} SymbolTable;
-
 extern Symbol* symbol_table_head;
 
 /* functions */
 void add_symbol(char* name, int address, int is_data, int is_extern);
 Symbol* find_symbol(const char* name);
-void report_error(const char* message, int line_num);
 void mark_entry(const char* name);
 void free_symbol_table(void);
+void print_symbol_table(void);
 
-/* NNEDS TO BE WRITTEN LATER!!!*/
-SymbolTable* create_symbol_table(void);
-void add_to_hash_table(SymbolTable* table, char* name, int address, int is_data, int is_extern);
-Symbol* find_in_hash_table(SymbolTable* table, const char* name);
-void free_hash_table(SymbolTable* table);
+
 
 #endif /* SYMBOL_TABLE_H */
