f15e9fdcbdbaba317aa0cd51d31843975da7d2d7
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-08-01 22:56:11 +0300

    Update middle_error.h

--- middle_error.h
+++ middle_error.h
@@ -1,12 +1,12 @@
 #ifndef MIDDLE_ERROR_H
 #define MIDDLE_ERROR_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/*report when a symbol is already defiend*/
 void symbol_already_defined_error(const char* name);
-void entry_on_external_error(const char* name);
+void invalid_label_error(const char* name);
+void label_on_extern_error(const char* label);
+void unknown_opcode_error(int opcode);
+void invalid_operand_type_error(const char* msg);
+void wrong_operand_count_error(int expected, int actual);
+void middle_error(const char* format, ...);
 
-#endif /* MIDDLE_ERROR_H */
+#endif /* MIDDLE_ERROR_ H*/
