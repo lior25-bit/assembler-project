cdbcd327ada69e17602737764a838cc4c23f16b8
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-08-01 22:02:30 +0300

    Update middle_error.h

--- middle_error.h
+++ middle_error.h
@@ -1,7 +1,12 @@
 #ifndef MIDDLE_ERROR_H
 #define MIDDLE_ERROR_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /*report when a symbol is already defiend*/
 void symbol_already_defined_error(const char* name);
+void entry_on_external_error(const char* name);
 
 #endif /* MIDDLE_ERROR_H */
