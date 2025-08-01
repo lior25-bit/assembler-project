adf714eb610cecfde11009f52e1b21b38c6dcca3
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-08-01 22:02:01 +0300

    Update symbol_table.h

--- symbol_table.h
+++ symbol_table.h
@@ -1,23 +1,30 @@
 #ifndef SYMBOL_TABLE_H
 #define SYMBOL_TABLE_H
 
+
+typedef enum {
+    SYMBOL_CODE,  /* instruction */
+    SYMBOL_DATA,  /* directive */
+    SYMBOL_EXTERN /* extern */
+} SymbolType;
+
 typedef struct Symbol {
     char* name;
     int address;
-    int is_data;
-    int is_extern;
     int is_entry;
-    struct Symbol* next;
+    SymbolType type;
+    struct Symbol* next; 
 } Symbol;
  
 extern Symbol* symbol_table_head;
 
 /* functions */
-void add_symbol(char* name, int address, int is_data, int is_extern);
+void add_symbol(char* name, int address, SymbolType type);
 Symbol* find_symbol(const char* name);
 void mark_entry(const char* name);
 void free_symbol_table();
 void print_symbol_table();
+void adjust_data_symbols(int instructions_size);
 
 
 
