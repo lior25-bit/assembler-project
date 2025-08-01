5f06e1d63e542d006c7b749dd1750630b5af8c05
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-08-01 22:02:52 +0300

    Update middle_error.c

--- middle_error.c
+++ middle_error.c
@@ -1,6 +1,9 @@
 #include "middle_error.h"
-#include <stdio.h>
 
 void symbol_already_defined_error(const char* name) {
     fprintf(stderr, "Error: Symbol '%s' is already defined.\n", name);
+}
+
+void entry_on_external_error(const char* name) {
+    fprintf(stderr, "Error: Symbol '%s' cannot be defined as both entry and extern.\n", name);
 }
