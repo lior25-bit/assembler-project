fc34def3db4a8d0defb95c25faeaaf4b7b8804c8
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-08-04 19:22:46 +0300

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MEMORY_START 100
+#define MAX_MEMORY 4096
+#define MAX_LABEL 30
+
+#define ARE_SHIFT 2
+#define ARE_A 0b00
+#define ARE_E 0b01
+#define ARE_R 0b10
+
+#endif
