cdb180f95d0a4b24b65d7ffeb7e85c44c6f84bb5
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-08-18 22:43:05 +0300

    Create memory_manager.h

---
+++ memory_manager.h
@@ -0,0 +1,27 @@
+#ifndef MEMORY_MANAGER_H
+#define MEMORY_MANAGER_H
+
+#include "constants.h"
+#include "error_manager.h"
+
+typedef struct ExternUsage {
+    char symbol_name[MAX_SYMBOL_NAME];
+    int address;
+    struct ExternUsage* next;
+} ExternUsage;
+
+typedef struct {
+    int IC;
+    int DC;
+    int code_image[MAX_MEMORY];
+    int data_image[MAX_MEMORY];
+    ExternUsage* extern_list;
+} MemoryManager;
+
+MemoryManager* create_memory_manager();
+void initialize_memory_manager(MemoryManager* memory);
+void add_extern_usage(MemoryManager* memory, const char* symbol_name, int address);
+void free_extern_list(ExternUsage* extern_list);
+void free_memory_manager(MemoryManager* memory);
+
+#endif
