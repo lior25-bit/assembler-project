faad85a34563b4379bc5a2135a82fc416671a649
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-08-18 22:44:52 +0300

    Update symbol_table.c

--- symbol_table.c
+++ symbol_table.c
@@ -1,28 +1,135 @@
 #include "symbol_table.h"
-#include "middle_error.h"
+#include "ast_logic.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-
-Symbol* symbol_table_head = NULL;
-
-void add_symbol(char* name, int address, SymbolType type) {
-    if(find_symbol(name) != NULL) {
-        symbol_alrady_defiend_error(name);
-        return;
-    }
-
-    Symbol* new_sym = malloc(sizeof(Symbol));
-    new_sym->name = strdup(name);
-    new_sym->address = address;
-    new_sym->is_entry = 0;
-    new_sym->type = type;
-
-    new_sym->next = symbol_table_head;
-    symbol_table_head = new_sym;
+int main() {
+    printf("symbol Table compiled successfully!\n");
+    return 0;
 }
 
-Symbol* find_symbol(const char* name) {
-    Symbol* current = symbol_table_head;
+Symbol* create_symbol(const char* name, int address, SymbolType type) {
+    Symbol* symbol = calloc(1, sizeof(Symbol));
+    if(!symbol) {
+        return NULL;
+    }
+    strcpy(symbol->name, name);
+    symbol->address = address;
+    symbol->type = type;
+    symbol->is_entry = 0;
+    symbol->next = NULL;
+    return symbol;
+}
 
+SymbolTable* create_empty_table() {
+    SymbolTable* table = calloc(1, sizeof(SymbolTable));
+    return table;
+}
+
+int is_valid_table(SymbolTable* table) {
+    Symbol* current;
+    if(!table) {
+        return 0;
+    }
+    current = table->head;
+    while(current != NULL) {
+        if(!is_valid_symbol(current, table)) {
+            return 0;
+        }
+        current = current->next;
+    }
+    return 1;
+}
+
+int is_valid_symbol(Symbol* Symbol, SymbolTable* table) {
+    char* name;
+    int address;
+    if(!Symbol || !table) {
+        return 0;
+    }
+    name = Symbol->name;
+    address = Symbol->address;
+    if(find_symbol(table, name)) {
+        return 0;
+    }
+    if(!is_valid_symbol_name(name)) {
+        return 0;
+    }
+    if(!is_valid_address(address)) {
+        return 0;
+    }
+    if(Symbol->type == SYMBOL_EXTERN && Symbol->is_entry == 1) {
+        return 0;
+    }
+    return 1;
+}
+
+int is_valid_symbol_name(const char* name) {
+    if(!name) {
+        return 0;
+    }
+    if(strlen(name) > MAX_SYMBOL_NAME) {
+        return 0;
+    }
+    if(!isalpha(name[0])) {
+        return 0;
+    }
+    if(is_reserved_word(name)) {
+        return 0;
+    }
+    if(!is_valid_char_symbol_name(name)) {
+        return 0;
+    }
+    return 1;
+}
+
+int is_reserved_word(const char* name) {
+    const char* reserved_list[] = {
+        "mov", "cmp", "add", "sub", "lea",
+        "clr", "not", "inc", "dec",
+        "jmp", "bne", "jsr", "red", "prn", "rts", "stop",
+        "data", "string", "mat", "entry", "extern",
+        "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7"
+    };
+    int count = (sizeof(reserved_list) / sizeof(reserved_list[0]));
+    int i;
+    for(i = 0; i < count; i++) {
+        if(strcmp(name, reserved_list[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int is_valid_char_symbol_name(const char* name) {
+    int i;
+    if(!name) {
+        return 0;
+    }
+    for(i = 1; name[i] != '\0'; i++) {
+        if(!isalnum(name[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int is_valid_address(int address) {
+    return (address >= MIN_ADDRESS && address <= MAX_ADDRESS);
+}
+
+int is_empty_table(SymbolTable* table) {
+    return (!table || table->head == NULL);
+}
+
+Symbol* find_symbol(SymbolTable* table, const char* name) {
+    Symbol* current;
+    if(!table || !name) {
+        return NULL;
+    }
+    current = table->head;
     while (current != NULL) {
         if (strcmp(current->name, name) == 0)
             return current;
@@ -31,66 +138,85 @@ Symbol* find_symbol(const char* name) {
     return NULL;
 }
 
-/* Updates every data type Symbol, The function gets the "size" the instructions occupied in the memory */
-void adjust_data_symbols(int instructions_size) {
-    Symbol* current = symbol_table_head;
+void mark_entry(SymbolTable* table, const char* name) {
+    Symbol* symbol = find_symbol(table, name);
+    if (symbol) {
+        symbol->is_entry = 1;
+    }
+}
+
+int is_entry_symbol(Symbol* symbol) {
+    return (symbol && symbol->is_entry);
+}
+
+SymbolTable* add_symbol_head(SymbolTable* table, Symbol* head) {
+    if(!table || !head) {
+        return NULL;
+    }
+    head->next = table->head;
+    table->head = head;
+    if(!table->last) {
+        table->last = head;
+    }
+    return table;
+}
+
+SymbolTable* add_last_symbol(SymbolTable* table, Symbol* last) {
+    if(!table || !last) {
+        return NULL;
+    }
+    if(table->last == NULL) {
+        table->head = last;
+        table->last = last;
+    } else {
+        table->last->next = last;
+        table->last = last;
+    }
+    last->next = NULL;
+    return table;
+}
+
+void update_data_symbols(SymbolTable* table, int ic) {
+    Symbol* current;
+    if(!table) {
+        return;
+    }
+    current = table->head;
     while(current != NULL ) {
-        if(current->type == SYMBOL_DATA)  { 
-            current->address += instructions_size;
+        if(current->type == SYMBOL_DATA)  {
+            current->address += ic;
         }
-      current = current->next;
-    }
- return;
-}
-
-void mark_entry(const char* name) {
-    Symbol* sym = find_symbol(name);
-    if (sym != NULL) {
-        if (sym->type == SYMBOL_EXTERN) {
-            entry_on_external_error(name); 
-            return;
-        }
-        sym->is_entry = 1;
+        current = current->next;
     }
 }
 
-
-void free_symbol_table() {
-    Symbol* current = symbol_table_head;
+void free_symbol_table(SymbolTable* table) {
+    Symbol* current;
+    if(!table) {
+        return;
+    }
+    current = table->head;
     while (current != NULL) {
         Symbol* temp = current;
         current = current->next;
-        free(temp->name);
         free(temp);
     }
-    symbol_table_head = NULL;
+    table->head = NULL;
+    table->last = NULL;
+    free(table);
 }
 
-/* used for debug purposes */
-void print_symbol_table() {
-    Symbol* current = symbol_table_head;
-    while (current != NULL) {
-        printf("Name: %s\n", current->name);
-
-        if (current->type == SYMBOL_DATA) {
-            printf("Type: DATA\n");
-        } else {
-            printf("Type: CODE\n");
-        }
-
-        if (current->is_entry) {
-            printf("Symbol is entry\n");
-        } else {
-            printf("Symbol is not entry\n");
-        }
-
-        if (current->type == SYMBOL_EXTERN) {
-            printf("Symbol is extern\n");
-        } else {
-            printf("Symbol is not extern\n");
-        }
-
-        printf("------------------------\n"); 
+int get_symbol_count(SymbolTable* table) {
+    Symbol* current;
+    int count;
+    if(!table) {
+        return 0;
+    }
+    current = table->head;
+    count = 0;
+    while(current != NULL) {
+        count++;
         current = current->next;
     }
+    return count;
 }
