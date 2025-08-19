59dfb4bd09f568028bc1fda0af5883b9b019d00b
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-08-19 05:29:17 +0300

    ALL DONE

--- first_pass.h
+++ first_pass.h
@@ -1,12 +1,12 @@
+/* This header contains the needed functions and includes for first pass management */
+
 #ifndef FIRST_PASS_H
 #define FIRST_PASS_H
 
-/* Heathers needed for compilation */
+#include "ast.h"
+#include "symbol_table.h"
 #include "error_manager.h"
 #include "memory_manager.h"
-#include "symbol_table.h"
-#include "ast.h"
-
 
 /* Function Declarations */
 
@@ -18,15 +18,16 @@ int process_node(ASTNode* node, MemoryManager* memory, SymbolTable* table, Error
 int process_instruction_node(ASTNode* node, MemoryManager* memory, ErrorManager* error_mgr);
 int process_directive_node(ASTNode* node, MemoryManager* memory, ErrorManager* error_mgr);
 SymbolType process_node_type(ASTNode* node);
-int is_valid_addressing_mode(OpCode op, ArgType src, ArgType dest, ErrorManager* error_mgr, int line);
+int is_valid_addressing_mode(OpCode op, ArgType src, ArgType dest, ErrorManager* error_mgr, const char* og_line);
 int validate_basic_node_params(ASTNode* node, MemoryManager* memory, ErrorManager* error_mgr);
 int validate_full_node_params(ASTNode* node, MemoryManager* memory, SymbolTable* table, ErrorManager* error_mgr);
 int handle_node_label(ASTNode* node, MemoryManager* memory, SymbolTable* table, ErrorManager* error_mgr);
 int calculate_instruction_size(ASTNode* node, ErrorManager* error_mgr);
 int calculate_matrix_memory(int rows, int cols);
 
+/* directive handlers */
+int handle_data_directive(ASTNode* node, MemoryManager* memory, ErrorManager* error_mgr);
+int handle_string_directive(ASTNode* node, MemoryManager* memory, ErrorManager* error_mgr);
+int handle_mat_directive(ASTNode* node, MemoryManager* memory, ErrorManager* error_mgr);
+
 #endif
-
-
-
-
