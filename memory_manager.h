eaf3b70bbd22804834be1a0f5176cef09b00c6c8
Lir Vine <105906523+lirv3@users.noreply.github.com>
2025-08-19 05:09:11 +0300

    ALL DONE

--- memory_manager.h
+++ memory_manager.h
@@ -1,8 +1,9 @@
+/* This heather contains the structs and functions needed for memory manegment */
+
 #ifndef MEMORY_MANAGER_H
 #define MEMORY_MANAGER_H
 
 #include "constants.h"
-#include "error_manager.h"
 
 typedef struct ExternUsage {
     char symbol_name[MAX_SYMBOL_NAME];
@@ -18,7 +19,7 @@ typedef struct {
     ExternUsage* extern_list;
 } MemoryManager;
 
-MemoryManager* create_memory_manager();
+MemoryManager* create_memory_manager(void);
 void initialize_memory_manager(MemoryManager* memory);
 void add_extern_usage(MemoryManager* memory, const char* symbol_name, int address);
 void free_extern_list(ExternUsage* extern_list);
